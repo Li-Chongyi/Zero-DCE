3689e11112cb90e31fab623b3f501e326ffe1a74
lujulia <39236354+lujulia@users.noreply.github.com>
2022-12-07 11:58:33 +0800

    Update model.py

--- Zero-DCE_code/model.py
+++ Zero-DCE_code/model.py
@@ -5,10 +5,10 @@ import math
 #import pytorch_colors as colors
 import numpy as np
 
-class enhance_net_nopool(nn.Module):
+class enhance_net_nopool_3(nn.Module):
 
 	def __init__(self):
-		super(enhance_net_nopool, self).__init__()
+		super(enhance_net_nopool_3, self).__init__()
 
 		self.relu = nn.ReLU(inplace=True)
 
@@ -57,3 +57,186 @@ class enhance_net_nopool(nn.Module):
 
 
 
+
+
+
+
+class enhance_net_nopool_1_1(nn.Module):
+
+	def __init__(self):
+		super(enhance_net_nopool_1_1, self).__init__()
+
+		self.relu = nn.ReLU(inplace=True)
+
+		number_f = 32
+		self.e_conv1 = nn.Conv2d(1,number_f,3,1,1,bias=True) 
+		self.e_conv2 = nn.Conv2d(number_f,number_f,3,1,1,bias=True) 
+		self.e_conv3 = nn.Conv2d(number_f,number_f,3,1,1,bias=True) 
+		self.e_conv4 = nn.Conv2d(number_f,number_f,3,1,1,bias=True) 
+		self.e_conv5 = nn.Conv2d(number_f*2,number_f,3,1,1,bias=True) 
+		self.e_conv6 = nn.Conv2d(number_f*2,number_f,3,1,1,bias=True) 
+		self.e_conv7 = nn.Conv2d(number_f*2,8,3,1,1,bias=True) 
+
+		self.maxpool = nn.MaxPool2d(2, stride=2, return_indices=False, ceil_mode=False)
+		self.upsample = nn.UpsamplingBilinear2d(scale_factor=2)
+
+
+		
+	def forward(self, X):
+		#print("forward: ",X.size())
+		#print("forward: ",X[:,:1,:,:].size())
+		
+		y = X[:,:1,:,:]
+		x1 = self.relu(self.e_conv1(y))
+		# p1 = self.maxpool(x1)
+		x2 = self.relu(self.e_conv2(x1))
+		# p2 = self.maxpool(x2)
+		x3 = self.relu(self.e_conv3(x2))
+		# p3 = self.maxpool(x3)
+		x4 = self.relu(self.e_conv4(x3))
+
+		x5 = self.relu(self.e_conv5(torch.cat([x3,x4],1)))
+		# x5 = self.upsample(x5)
+		x6 = self.relu(self.e_conv6(torch.cat([x2,x5],1)))
+
+		x_r = F.tanh(self.e_conv7(torch.cat([x1,x6],1)))
+		#print("x_r: ",x_r.size())
+		r1,r2,r3,r4,r5,r6,r7,r8 = torch.split(x_r, 1, dim=1)
+		#print("r1: ",r1.size())
+
+		x = y + r1*(torch.pow(y,2)-y)
+		x = x + r2*(torch.pow(x,2)-x)
+		x = x + r3*(torch.pow(x,2)-x)
+		enhance_image_1 = x + r4*(torch.pow(x,2)-x)
+		#print("enhance_image_1: ",enhance_image_1.size())
+		#print("enhance_image_1: ",enhance_image_1[:,0,:,:].size())
+		#print("enhance_image_1: ",X[:,2:,:,:].size())
+		enhance_image_2 = torch.cat([enhance_image_1,X[:,1:2,:,:],X[:,2:,:,:]],1)
+		x = enhance_image_1 + r5*(torch.pow(enhance_image_1,2)-enhance_image_1)		
+		x = x + r6*(torch.pow(x,2)-x)	
+		x = x + r7*(torch.pow(x,2)-x)
+		enhance_image = x + r8*(torch.pow(x,2)-x)
+		#print("enhance_image: ",enhance_image_1.size())
+		enhance_image_3 = torch.cat([enhance_image,X[:,1:2,:,:],X[:,2:,:,:]],1)
+		r = torch.cat([r1,r2,r3,r4,r5,r6,r7,r8],1)
+		return enhance_image_2,enhance_image_3,r
+
+class enhance_net_nopool_1_2(nn.Module):
+
+	def __init__(self):
+		super(enhance_net_nopool_1_2, self).__init__()
+
+		self.relu = nn.ReLU(inplace=True)
+
+		number_f = 32
+		self.e_conv1 = nn.Conv2d(1,number_f,3,1,1,bias=True) 
+		self.e_conv2 = nn.Conv2d(number_f,number_f,3,1,1,bias=True) 
+		self.e_conv3 = nn.Conv2d(number_f,number_f,3,1,1,bias=True) 
+		self.e_conv4 = nn.Conv2d(number_f,number_f,3,1,1,bias=True) 
+		self.e_conv5 = nn.Conv2d(number_f*2,number_f,3,1,1,bias=True) 
+		self.e_conv6 = nn.Conv2d(number_f*2,number_f,3,1,1,bias=True) 
+		self.e_conv7 = nn.Conv2d(number_f*2,8,3,1,1,bias=True) 
+
+		self.maxpool = nn.MaxPool2d(2, stride=2, return_indices=False, ceil_mode=False)
+		self.upsample = nn.UpsamplingBilinear2d(scale_factor=2)
+
+
+		
+	def forward(self, X):
+		#print("forward: ",X.size())
+		#print("forward: ",X[:,:1,:,:].size())
+		
+		y = X[:,1:2,:,:]
+		x1 = self.relu(self.e_conv1(y))
+		# p1 = self.maxpool(x1)
+		x2 = self.relu(self.e_conv2(x1))
+		# p2 = self.maxpool(x2)
+		x3 = self.relu(self.e_conv3(x2))
+		# p3 = self.maxpool(x3)
+		x4 = self.relu(self.e_conv4(x3))
+
+		x5 = self.relu(self.e_conv5(torch.cat([x3,x4],1)))
+		# x5 = self.upsample(x5)
+		x6 = self.relu(self.e_conv6(torch.cat([x2,x5],1)))
+
+		x_r = F.tanh(self.e_conv7(torch.cat([x1,x6],1)))
+		#print("x_r: ",x_r.size())
+		r1,r2,r3,r4,r5,r6,r7,r8 = torch.split(x_r, 1, dim=1)
+		#print("r1: ",r1.size())
+
+		x = y + r1*(torch.pow(y,2)-y)
+		x = x + r2*(torch.pow(x,2)-x)
+		x = x + r3*(torch.pow(x,2)-x)
+		enhance_image_1 = x + r4*(torch.pow(x,2)-x)
+		#print("enhance_image_1: ",enhance_image_1.size())
+		#print("enhance_image_1: ",enhance_image_1[:,0,:,:].size())
+		#print("enhance_image_1: ",X[:,2:,:,:].size())
+		enhance_image_2 = torch.cat([X[:,:1,:,:],enhance_image_1,X[:,2:,:,:]],1)
+		x = enhance_image_1 + r5*(torch.pow(enhance_image_1,2)-enhance_image_1)		
+		x = x + r6*(torch.pow(x,2)-x)	
+		x = x + r7*(torch.pow(x,2)-x)
+		enhance_image = x + r8*(torch.pow(x,2)-x)
+		#print("enhance_image: ",enhance_image_1.size())
+		enhance_image_3 = torch.cat([X[:,:1,:,:],enhance_image,X[:,2:,:,:]],1)
+		r = torch.cat([r1,r2,r3,r4,r5,r6,r7,r8],1)
+		return enhance_image_2,enhance_image_3,r
+
+class enhance_net_nopool_1_3(nn.Module):
+
+	def __init__(self):
+		super(enhance_net_nopool_1_3, self).__init__()
+
+		self.relu = nn.ReLU(inplace=True)
+
+		number_f = 32
+		self.e_conv1 = nn.Conv2d(1,number_f,3,1,1,bias=True) 
+		self.e_conv2 = nn.Conv2d(number_f,number_f,3,1,1,bias=True) 
+		self.e_conv3 = nn.Conv2d(number_f,number_f,3,1,1,bias=True) 
+		self.e_conv4 = nn.Conv2d(number_f,number_f,3,1,1,bias=True) 
+		self.e_conv5 = nn.Conv2d(number_f*2,number_f,3,1,1,bias=True) 
+		self.e_conv6 = nn.Conv2d(number_f*2,number_f,3,1,1,bias=True) 
+		self.e_conv7 = nn.Conv2d(number_f*2,8,3,1,1,bias=True) 
+
+		self.maxpool = nn.MaxPool2d(2, stride=2, return_indices=False, ceil_mode=False)
+		self.upsample = nn.UpsamplingBilinear2d(scale_factor=2)
+
+
+		
+	def forward(self, X):
+		#print("forward: ",X.size())
+		#print("forward: ",X[:,:1,:,:].size())
+		
+		y = X[:,2:,:,:]
+		x1 = self.relu(self.e_conv1(y))
+		# p1 = self.maxpool(x1)
+		x2 = self.relu(self.e_conv2(x1))
+		# p2 = self.maxpool(x2)
+		x3 = self.relu(self.e_conv3(x2))
+		# p3 = self.maxpool(x3)
+		x4 = self.relu(self.e_conv4(x3))
+
+		x5 = self.relu(self.e_conv5(torch.cat([x3,x4],1)))
+		# x5 = self.upsample(x5)
+		x6 = self.relu(self.e_conv6(torch.cat([x2,x5],1)))
+
+		x_r = F.tanh(self.e_conv7(torch.cat([x1,x6],1)))
+		#print("x_r: ",x_r.size())
+		r1,r2,r3,r4,r5,r6,r7,r8 = torch.split(x_r, 1, dim=1)
+		#print("r1: ",r1.size())
+
+		x = y + r1*(torch.pow(y,2)-y)
+		x = x + r2*(torch.pow(x,2)-x)
+		x = x + r3*(torch.pow(x,2)-x)
+		enhance_image_1 = x + r4*(torch.pow(x,2)-x)
+		#print("enhance_image_1: ",enhance_image_1.size())
+		#print("enhance_image_1: ",enhance_image_1[:,0,:,:].size())
+		#print("enhance_image_1: ",X[:,2:,:,:].size())
+		enhance_image_2 = torch.cat([X[:,:1,:,:],X[:,1:2,:,:],enhance_image_1],1)
+		x = enhance_image_1 + r5*(torch.pow(enhance_image_1,2)-enhance_image_1)		
+		x = x + r6*(torch.pow(x,2)-x)	
+		x = x + r7*(torch.pow(x,2)-x)
+		enhance_image = x + r8*(torch.pow(x,2)-x)
+		#print("enhance_image: ",enhance_image_1.size())
+		enhance_image_3 = torch.cat([X[:,:1,:,:],X[:,1:2,:,:],enhance_image],1)
+		r = torch.cat([r1,r2,r3,r4,r5,r6,r7,r8],1)
+		return enhance_image_2,enhance_image_3,r
