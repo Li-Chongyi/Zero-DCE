a2ab5ef2e0e49197d0e53271c5fcea9258e67360
lujulia <39236354+lujulia@users.noreply.github.com>
2022-12-09 07:56:20 +0800

    Update lowlight_test.py

--- Zero-DCE_code/lowlight_test.py
+++ Zero-DCE_code/lowlight_test.py
@@ -24,9 +24,9 @@ import tensorflow as tf
 	
 
 
-def Color_Choice(color_space,data_lowlight_path):
+def Color_Choice(color_space,data_lowlight):
     #data_lowlight = Image.open(data_lowlight_path).convert(color)
-    data_lowlight = cv2.imread(data_lowlight_path)
+    #data_lowlight = cv2.imread(data_lowlight_path)
     data_lowlight = cv2.cvtColor(data_lowlight, cv2.COLOR_BGR2RGB)
     if color_space == "RGB":
         data_lowlight = (np.asarray(data_lowlight)/255.0)
@@ -47,13 +47,13 @@ def Color_Choice(color_space,data_lowlight_path):
             back = cv2.COLOR_YCrCb2BGR
         elif color_space == "YUV":
             data_lowlight = cv2.cvtColor(data_lowlight, cv2.COLOR_RGB2YUV)
-            n = [235-16,16,235-16,16,235-16,16]
+            n = [255,0,255,0,255,0]#n = [235-16,16,255,0,255,0]#n = [235-16,16,240-16,16,240-16,16]#n = [235-16,16,235-16,16,235-16,16]
             back = cv2.COLOR_YUV2BGR
         elif color_space == "LAB":
             data_lowlight = cv2.cvtColor(data_lowlight, cv2.COLOR_RGB2Lab)
-            n = [255-1,1,255-1,1,255-1,1]
+            n = [255,0,255-1,1,255-1,1]
             back = cv2.COLOR_Lab2BGR
-        elif color_space == "Luv":
+        elif color_space == "LUV":
             data_lowlight = cv2.cvtColor(data_lowlight, cv2.COLOR_RGB2Luv)
             n = [255,0,255,0,255,0]
             back = cv2.COLOR_Luv2BGR
@@ -69,20 +69,19 @@ def Color_Choice(color_space,data_lowlight_path):
 
 
 
-def lowlight(color_channel,lowlight_images_path,save_images_path):
+def lowlight(color_channel,lowlight_image):
     os.environ['CUDA_VISIBLE_DEVICES']='0'
     
-    data_lowlight,con,inchan = Color_Choice(color_channel,lowlight_images_path)
+    data_lowlight,con,inchan = Color_Choice(color_channel,lowlight_image)
     data_lowlight = data_lowlight.cuda().unsqueeze(0)
     
     if config.channel=="RGB":
         DCE_net = model.enhance_net_nopool_3().cuda()
     elif config.channel=="HSV":
         DCE_net = model.enhance_net_nopool_1_3().cuda()
-
     elif config.channel=="HLS":
         DCE_net = model.enhance_net_nopool_1_2().cuda()
-    elif config.channel=="YCbCr" or  config.channel=="YUV" or  config.channel=="LAB" or  config.channel=="Luv":
+    elif config.channel=="YCbCr" or  config.channel=="YUV" or  config.channel=="LAB" or  config.channel=="LUV":
         DCE_net = model.enhance_net_nopool_1_1().cuda()
     
     DCE_net.load_state_dict(torch.load("snapshots/"+color_channel+".pth"))
@@ -91,20 +90,17 @@ def lowlight(color_channel,lowlight_images_path,save_images_path):
     _,enhanced_image,_ = DCE_net(data_lowlight)
     data_lowlight = enhanced_image[0].permute(1,2,0).cpu().numpy()
     temp1 = np.zeros((data_lowlight[:,:,0].shape[0],data_lowlight[:,:,0].shape[1]), dtype="uint8")
-    temp2= np.zeros((data_lowlight[:,:,0].shape[0],data_lowlight[:,:,0].shape[1]), dtype="uint8")
+    temp2 = np.zeros((data_lowlight[:,:,0].shape[0],data_lowlight[:,:,0].shape[1]), dtype="uint8")
     temp3 = np.zeros((data_lowlight[:,:,0].shape[0],data_lowlight[:,:,0].shape[1]), dtype="uint8")
     temp1[:,:] = (data_lowlight[:,:,0]*con[0]+con[1]).astype(dtype="uint8")
     temp2[:,:] = (data_lowlight[:,:,1]*con[2]+con[3]).astype(dtype="uint8")
     temp3[:,:] = (data_lowlight[:,:,2]*con[4]+con[5]).astype(dtype="uint8")
 
-    data_lowlight = cv2.cvtColor(cv2.merge([temp1,temp2,temp3]), inchan)
-    result_path = lowlight_images_path.replace('test_data','result')
-    if not os.path.exists(result_path.replace('/'+result_path.split("/")[-1],'')):
-        os.makedirs(result_path.replace('/'+result_path.split("/")[-1],''))
-    cv2.imwrite(result_path,data_lowlight)
+    data_enhanced = cv2.cvtColor(cv2.merge([temp1,temp2,temp3]), inchan)
     
     end_time = (time.time() - start)
     print(end_time)
+    return data_enhanced
     """
     result_path = lowlight_images_path.replace('test_data','result')
     if not os.path.exists(result_path.replace('/'+result_path.split("/")[-1],'')):
@@ -123,17 +119,40 @@ if __name__ == '__main__':
     parser.add_argument("--save_images_path", type=str, default="data/result")
     config = parser.parse_args()
     with torch.no_grad():
+        file_path = config.lowlight_images_path
+        file_list = os.listdir(config.lowlight_images_path)
         if config.mode == "image":
-            file_path = config.lowlight_images_path
-            file_list = os.listdir(config.lowlight_images_path)
             for file_name in file_list:
                 test_list = glob.glob(file_path+file_name+"/*") 
-                for image in test_list:
+                for image_path in test_list:
 				# image = image
-                    print(image)
-                    lowlight(config.channel,image,config.save_images_path)
-        #elif config.mode == "video":
-
-
-		
-
+                    print(image_path)
+                    data_lowlight = cv2.imread(image_path)
+                    data_enhanced = lowlight(config.channel,data_lowlight)
+                    result_path = image_path.replace('test_data','result')
+                    if not os.path.exists(result_path.replace('/'+result_path.split("/")[-1],'')):
+                        os.makedirs(result_path.replace('/'+result_path.split("/")[-1],''))
+                    cv2.imwrite(result_path,data_enhanced)
+        elif config.mode == "video":
+            for file_name in file_list:
+                test_list = glob.glob(file_path+file_name+"/*") 
+                for video_path in test_list:
+                    cap = cv2.VideoCapture(video_path)
+                    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
+                    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float
+                    fps = cap.get(cv2.CAP_PROP_FPS)
+                    result_path = video_path.replace('test_data','result')
+                    if not os.path.exists(result_path.replace('/'+result_path.split("/")[-1],'')):
+                        os.makedirs(result_path.replace('/'+result_path.split("/")[-1],''))
+                    vid_writer = cv2.VideoWriter(
+			            result_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (int(width), int(height))
+			        )
+                    while True:
+                        ret_val, frame = cap.read()
+                        if ret_val:
+                            frame = np.array(frame)
+                            lowlight(config.channel,frame)
+                            frame_enhanced = lowlight(config.channel,frame)
+                            vid_writer.write(frame_enhanced)
+                        else:
+                            break
